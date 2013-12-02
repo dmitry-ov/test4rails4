61a484b04137517eaa7864477b9ef78782a37587
dmitry <ovcharenkodmitry@gmail.com>
2013-12-01 23:55:35 -0800

    Получение названий и расписания работает

--- get_info.rb
+++ get_info.rb
@@ -1,35 +1,29 @@
-# //*[@id="rt-mainbody"]/div/div[7]
-# //*[@id="rt-mainbody"]/div/div[8]
-#af_category_item
-
-#  Заходим на страницу http://www.horizont-cinema.ru/
+# Заходим на страницу http://www.horizont-cinema.ru/
 # Получаем названия фильмов и расписания.(время --- цена)
 # Сохраняем себе в базу
 #
 #
 
+
 agent = Mechanize.new
 
-# находим элементы расписания
 items = agent.get('http://www.horizont-cinema.ru').search(".//div[@class='af_category_item']")
 
-# в каждом элементе находим название фильма
-title = []
-shedule = []
+shedule = {}
 
 items.each do |item|
-	title << item.search(".//div[@class='event_name']").children.children.children.text
-	timetable = item.search(".//span[@class='inpast']")
-   
+    title = item.search(".//div[@class='event_name']").children.children.children.text
+    
+    timetable = item.search(".//span[@id='msg_rpice']")
+
+    shedule_title = [] 
     timetable.each do |row|
-		# время начала сеанса
-		time = row.children[0].text
-		# цена билета
-		price = row.children[1].children[2]
-		shedule << "#{time} #{price}"  
+        time = row.children[0].text
+        price = row.children[1].children[2].text
+        shedule_title << "#{time} #{price}"
     end
+    shedule.merge!({title => shedule_title})
 end
-title
+
 shedule
-
-
+    
